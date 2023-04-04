4c9ae5d16379e753cb12025d5ec0a21015c3a97f
David Cohen <drcohen127@gmail.com>
2023-04-04 15:43:16 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const p= document.getElementById("title");
+const h2=document.getElementById("title2");
+const iframes=document.getElementsByTagName('iframe');
+
+
+
+//p.style.color='pink'
+//h2.style.color ="pink";
+// iframes[1].style.display='none'
+iframes[0].addEventListener('mouseover',click);
+
+// function click(){
+//     h2.style.color='green'
+//     p.style.color='green'
+    
+// }
